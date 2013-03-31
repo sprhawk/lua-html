da0f4fc6e068ab6439d4599d5160f3d3f7fe15e2
sprhawk <hongbo@yang.me>
2013-04-01 01:15:30 +0800

    changed test.lua

--- test.lua
+++ test.lua
@@ -3,7 +3,12 @@ require("html")
 --print(HTMLParser)
 --print(HTMLParser.newparser)
 
-file = io.open("sample.html")
+if #arg == 0 then
+   print("uasge:" .. arg[-1] .. " " .. arg[0] .. " sample.html")
+   return 
+end
+
+file = io.open(arg[1])
 str = file:read("*all")
 
 local doc = html.newdocument(str)
@@ -15,16 +20,14 @@ elem = doc:getElementById("userinfo_form");
 print(elem)
 print(elem:getAttribute("id"))
 
-local child = elem
+local elem = elem:copy()
+local child = elem:firstChild()
 repeat
    if 1 == child:type() then
       print("node name:", child:name())
    end
    local n = child:firstChild()
    if nil == n then
-      if child == elem then
-         break
-      end
       n = child:nextSibling()
       if nil == n then
          repeat 
@@ -40,10 +43,6 @@ repeat
    end
 until nil == child 
 
-
-
-   
-
 --[[
 print(parser)
 
@@ -58,4 +57,3 @@ for k,v in pairs(parser) do
    print(k)
    end
 ]]
-
